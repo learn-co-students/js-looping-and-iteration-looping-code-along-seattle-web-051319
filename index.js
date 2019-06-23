ec8aa458eb66688bc8d6af1e07243afb04a95913
Fabian <fabrofabian@gmail.com>
2019-06-23 10:11:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let theArray = []
+    for(let i = 0; i < names.length; i++){
+        theArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return theArray;
+}
+
+function countdown(count){
+    while(count > -1){
+        console.log(count);
+        count--;
+    }
+}
