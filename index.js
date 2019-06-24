f98958206828411b7c91e60c1918bc0682486711
fut0k0 <cccabell@icloud.com>
2019-06-24 15:19:52 -0700

    Passing lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards (names, event) {
+	const thanks = [];
+	
+	for (let i = 0; i < names.length; i++) {
+		thanks.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+	}
+	
+	return thanks;
+}
+
+function countdown (num) {
+	while (num >= 0) {
+		console.log(num);
+		num--;
+	}
+}
