d08fab42870e93316cb74b8b65e0c3362da48a98
AustinBH <harlowaustin2@gmail.com>
2019-06-23 21:56:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let newNames = []
+  for (let i = 0; i < names.length; i++) {
+    newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return newNames
+}
+
+function countdown(number) {
+  let i = 0
+  while (i <= number) {
+    console.log(number - i)
+    i++
+  }
+}
